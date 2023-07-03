2bcda5e877f1ca4a6a3d99304afb3c409a7662d0
zhoutao <immrtao@163.com>
2023-07-03 11:18:40 +0800

    修改default字段

--- js/background.js
+++ js/background.js
@@ -2,11 +2,11 @@
 chrome.storage.sync.get(['urlFrom'], function (result) {
   let urlFrom = result.urlFrom
   if (!urlFrom) {
-    urlFrom = 'default'
+    urlFrom = 'defaultOptions'
     // 设置不存在
     chrome.storage.sync.set({
       urlFrom,
-      default: 'https://cn.bing.com/',
+      defaultOptions: 'https://cn.bing.com/',
     })
   }
 })
