8f448d384ee9067f19eeb4c6afad80ac5ab95cdb
zhoutao <immrtao@163.com>
2023-07-03 11:16:05 +0800

    设置初始化

--- js/background.js
+++ js/background.js
@@ -1,15 +1,12 @@
-// 初始化新标签页
-chrome.storage.sync.get(['homeUrl', 'urlFrom'], function (result) {
-  let { homeUrl, urlFrom } = result
-
-  if (homeUrl === undefined) {
-    homeUrl = 'https://cn.bing.com/'
-    chrome.storage.sync.set({ homeUrl }, () => {
-      console.log('homeUrl is set to ' + homeUrl)
-    })
-  }
-  if (urlFrom === undefined) {
+// 初始化设置
+chrome.storage.sync.get(['urlFrom'], function (result) {
+  let urlFrom = result.urlFrom
+  if (!urlFrom) {
     urlFrom = 'default'
-    chrome.storage.sync.set({ urlFrom })
+    // 设置不存在
+    chrome.storage.sync.set({
+      urlFrom,
+      default: 'https://cn.bing.com/',
+    })
   }
 })
